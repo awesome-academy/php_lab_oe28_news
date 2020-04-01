9579a41a38456ac4e7903e5e2a58bdf91efb443e
ngoctho-hust <ngoctho.t1k54@gmail.com>
2020-04-01 09:25:50 +0700

    init model, add relationship, database

---
+++ app/Http/Models/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+    protected $fillable = [
+        'name',
+        'parent_id',
+    ];
+
+    public function news()
+    {
+        return $this->hasMany(Book::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+}
---
+++ database/migrations/2020_03_31_131915_create_news_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->text('content');
+            $table->tinyInteger('hot');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->tinyInteger('status');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
